8f5f77e2108c536a17c3b10b017742bd9449fe5b
Mas-Nez <79572670+tols-lol@users.noreply.github.com>
2022-09-03 23:40:31 +0700

    Update sewa.js

--- plugins/sewa.js
+++ plugins/sewa.js
@@ -9,14 +9,14 @@ let handler = async (m, { conn }) => {
   description: ` *• SEWA BOT & UP TO PREMIUM •*
         
 1. Grup / 30 Hari
-Rp. 15,000 Dana
-Rp. 20,000 Pulsa
+Rp. 5,000 Dana
+Rp. 5,000 Pulsa
 2. Premium / 30 Hari
-Rp. 10,000 Dana
-Rp. 10,000 Pulsa
+Rp. 1,000 Dana
+Rp. 1,000 Pulsa
 3. Premium + Grup / 30 Hari
-Rp. 20,000 Dana
-Rp. 20,000 Pulsa
+Rp. 6,000 Dana
+Rp. U,000 Pulsa
 wa.me/${owner[0]}
 *Bukan Bot!!!*
 *Owner ${conn.user.name}*
